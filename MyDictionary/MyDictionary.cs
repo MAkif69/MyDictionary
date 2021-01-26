81b87ade2c3b9ad0c4292b058e65b95d4a6d05df
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-01-26 21:00:06 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key,V value)
+        {
+            K[] tempKeys = keys;
+            keys = new K[keys.Length + 1];
+
+            V[] tempValues = values;
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + ":" + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(100, "Matemtaik Soru Bankası");
+            myDictionary.Add(101, "Tarih Soru Bankası");
+            myDictionary.Add(102, "Coğrafya Soru Bankası");
+
+            myDictionary.List();
+        }
+    }
+}
